e2253eec464e4b417764106fbf4cee5803133314
xpplusChile <129527670+xpplusChile@users.noreply.github.com>
2023-09-15 12:22:35 -0300

    Update check_if_send_data.sh

--- scripts/check_if_send_data.sh
+++ scripts/check_if_send_data.sh
@@ -4,8 +4,8 @@ ping -c 1 -q google.com >&/dev/null; echo $?
 
 if [ $? -eq 0 ]; then
     printf "\nonline\n" 
-    python sen_files_metadata.py
-    python pitaya.py
+    python sen_files_metadata.py #log con peso y cantidad de archivos mandados
+    python pitaya.py             #manda los archivos y los borra los mandados de la memoria interna
 else
     printf "\noffline\n" 
 fi
