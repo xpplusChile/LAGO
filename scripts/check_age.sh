5e37760873d3816210281b958466fd85900e2a67
xpplusChile <129527670+xpplusChile@users.noreply.github.com>
2023-09-15 12:32:59 -0300

    Update check_age.sh

--- scripts/check_age.sh
+++ scripts/check_age.sh
@@ -3,13 +3,13 @@
 age () { stat=$(stat --printf="%Y %F\n" "$1");days=$((($(date +%s) - ${stat%% *})/86400)) ;  }
 delete_age=2
 
-search_dir=/home/sebastian/Documents/Programas/lago/Automatizacion_lab_uls/red_pitaya/
-#search_dir=/home/src/logs/
+#search_dir=/home/sebastian/Documents/Programas/lago/Automatizacion_lab_uls/red_pitaya/
+search_dir=/home/src/logs/
 for entry in "$search_dir"/*
 do
 	age $entry
 	if [[ $days -ge $delete_age ]];then
 		echo $entry
-		#rm $entry
+		#rm $entry    #Con todo en orden se deja descomentado
 	fi
 done
